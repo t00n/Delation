ca49fc3f6411c098f848f0bc1c2d7de99cf2b94e
t00n <kelkununtel@hotmail.com>
2016-12-14 18:39:09 +0100

    [add] compute lines and comments

--- delation.py
+++ delation.py
@@ -9,6 +9,12 @@ class dict(dict):
         super().__init__(*args, **kwargs)
         self.__dict__ = self
 
+def walk_tree(f, tree):
+	res = f(tree)
+	for tree in tree.trees:
+		walk_tree(f, tree)
+
+
 @dispatch_command
 @arg('extension')
 @arg('comment')
@@ -18,8 +24,14 @@ def main(extension, comment, directory):
 	stats_by_user = defaultdict(lambda: dict(commits=0, lines=0, comments=0))
 	for commit in repo.commit().iter_items(repo, 'HEAD'):
 		stats_by_user[commit.committer.name].commits += 1
+	tree = repo.heads.master.commit.tree
+	def update_stats(tree):
+		for blob in tree.blobs:
+			for commit, lines in repo.blame('HEAD', blob.path):
+				stats_by_user[commit.committer.name].lines += len(lines)
+				stats_by_user[commit.committer.name].comments += len([line for line in lines 
+																		   if isinstance(line, str) 
+																		   and comment in line])
+	walk_tree(update_stats, tree)
 	print(stats_by_user)
-	# tree = repo.heads.master.commit.tree
-	# for blob in tree.blobs:
-	# 	print(blob.path)
 

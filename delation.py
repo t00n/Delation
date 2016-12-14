5fa75155119dfcd24c48536a3e418a7c40c179dc
t00n <kelkununtel@hotmail.com>
2016-12-14 18:15:42 +0100

    [enh] use dict trick coz it is so good

--- delation.py
+++ delation.py
@@ -4,16 +4,21 @@ from argh import *
 from git import *
 from collections import defaultdict
 
+class dict(dict):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.__dict__ = self
+
 @dispatch_command
 @arg('extension')
 @arg('comment')
 @arg('directory')
 def main(extension, comment, directory):
 	repo = Repo(directory)
-	commits_by_user = defaultdict(lambda: 0)
+	stats_by_user = defaultdict(lambda: dict(commits=0, lines=0, comments=0))
 	for commit in repo.commit().iter_items(repo, 'HEAD'):
-		commits_by_user[commit.committer.name] += 1
-	print(commits_by_user)
+		stats_by_user[commit.committer.name].commits += 1
+	print(stats_by_user)
 	# tree = repo.heads.master.commit.tree
 	# for blob in tree.blobs:
 	# 	print(blob.path)
